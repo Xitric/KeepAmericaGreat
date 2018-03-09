f8b644b116e6306b7839b7aaa396d626c67cb568
Mikkel Struntze <mikkel.struntze@gmail.com>
2018-03-09 10:56:12 +0100

    Pathfinder, Sound module init

---
+++ Sound/src/main/java/com/kag/sound/Sound.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kag.sound;
+
+import com.kag.common.interfaces.ISound;
+
+/**
+ *
+ * @author mstruntze
+ */
+public class Sound implements ISound {
+
+    @Override
+    public void playSound(String filepath) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void playRepeatingSound(String filepath) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stopSound(String filepath) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stopAllSounds() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
