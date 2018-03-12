84b109d3991ac00d10c36bc11e651fd40e3a4ff3
Unknown <2809thy@gmail.com>
2018-03-12 09:31:03 +0100

    Added GameData and Camera Modified World

---
+++ Common/src/main/java/com/kag/common/data/GameData.java
@@ -0,0 +1,53 @@
+package com.kag.common.data;
+
+public class GameData {
+
+    private final Keyboard keyboard;
+    private final Mouse mouse;
+    private final int width;
+    private final int height;
+    private final Camera camera;
+
+    public GameData(Keyboard keyboard, Mouse mouse, int width, int height, Camera camera) {
+	this.keyboard = keyboard;
+	this.mouse = mouse;
+	this.width = width;
+	this.height = height;
+	this.camera = camera;
+    }
+
+    /**
+     * @return the mouse
+     */
+    public Mouse getMouse() {
+	return mouse;
+    }
+
+    /**
+     * @return the width
+     */
+    public int getWidth() {
+	return width;
+    }
+
+    /**
+     * @return the height
+     */
+    public int getHeight() {
+	return height;
+    }
+
+    /**
+     * @return the camera
+     */
+    public Camera getCamera() {
+	return camera;
+    }
+
+    /**
+     * @return the keyboard
+     */
+    public Keyboard getKeyboard() {
+	return keyboard;
+    }
+}
--- Common/src/main/java/com/kag/common/data/World.java
+++ Common/src/main/java/com/kag/common/data/World.java
@@ -5,6 +5,8 @@
  */
 package com.kag.common.data;
 
+import com.kag.common.entities.Entity;
+
 /**
  *
  * @author Sofie Jørgensen
@@ -13,25 +15,45 @@ public class World {
 
     /**
      * isWalkable considers only Tile.isWalkable()
+     *
      * @param x The x-coordinate for the tile to check upon
      * @param y The y-coordinate for the tile to check upon
      * @return true if Tile.isWalkable is true, false otherwise
      */
     public boolean isWalkable(int x, int y) {
-        //Find a tile with the given x and y
-        //return Tile.isWalkable();
-        return false; 
+	//Find a tile with the given x and y
+	//return Tile.isWalkable();
+	return false;
     }
 
     /**
-     * isOccupied considers all entities with the BlockingPart and Tile.isWalkable()
+     * isOccupied considers all entities with the BlockingPart and
+     * Tile.isWalkable()
+     *
      * @param x The x-coordinate for the tile to check upon
      * @param y The y-coordinate for the tile to check upon
-     * @return true if Tile.isWalkable is false or if there's an entity on x,y with a BlockingPart
+     * @return true if Tile.isWalkable is false or if there's an entity on x,y
+     * with a BlockingPart
      */
     public boolean isOccupied(int x, int y) {
-        //if IsWalkable is false, return true
-        //else, loop through entities and check for BlockingPart
-        return false;
+	//if IsWalkable is false, return true
+	//else, loop through entities and check for BlockingPart
+	return false;
+    }
+
+    public void addEntity(Entity entity) {
+
+    }
+
+    public void removeEntity(Entity entity) {
+
+    }
+
+    public Entity[] getAllEntities() {
+	return null;
+    }
+	
+    public Entity getEntityAt(int screenX, int screenY){
+	return null;
     }
 }
