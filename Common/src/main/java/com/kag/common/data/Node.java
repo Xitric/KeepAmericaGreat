76a0a5ce90857edd5e6e4701bb9391e135063bde
André Thy <2809thy@gmail.com>
2018-03-12 09:35:06 +0100

    Added getters, constructur and made fields private

--- Common/src/main/java/com/kag/common/data/Node.java
+++ Common/src/main/java/com/kag/common/data/Node.java
@@ -1,6 +1,19 @@
 package com.kag.common.data;
 
 public class Node {
-    Node next;
-    Tile tile;
+    private Node next;
+    private Tile tile;
+
+    public Node(Tile tile, Node node){
+	this.next = node;
+	this.tile = tile;
+    }
+    
+    public Node getNext() {
+	return next;
+    }
+
+    public Tile getTile() {
+	return tile;
+    }
 }
