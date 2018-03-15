1bb789f22f0e1952fd083aaddd7120b96dccb04e
André Thy <2809thy@gmail.com>
2018-03-15 13:44:46 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- ModuleUpdateManager/src/main/java/com/kag/moduleupdatemanager/ModuleUpdateManager.java
+++ ModuleUpdateManager/src/main/java/com/kag/moduleupdatemanager/ModuleUpdateManager.java
@@ -14,84 +14,139 @@ import org.netbeans.api.autoupdate.InstallSupport.Validator;
 import org.netbeans.api.autoupdate.OperationContainer;
 import org.netbeans.api.autoupdate.OperationContainer.OperationInfo;
 import org.netbeans.api.autoupdate.OperationException;
+import org.netbeans.api.autoupdate.OperationSupport;
 import org.netbeans.api.autoupdate.OperationSupport.Restarter;
 import org.netbeans.api.autoupdate.UpdateElement;
 import org.netbeans.api.autoupdate.UpdateManager;
 import org.netbeans.api.autoupdate.UpdateUnit;
 import org.netbeans.api.autoupdate.UpdateUnitProvider;
 import org.netbeans.api.autoupdate.UpdateUnitProviderFactory;
-import org.openide.awt.NotificationDisplayer;
-import org.openide.util.ImageUtilities;
+import org.openide.util.Exceptions;
 
 public class ModuleUpdateManager {
 
     private List<UpdateElement> install = new ArrayList<>();
     private List<UpdateElement> update = new ArrayList<>();
+    private List<UpdateElement> localInstalled = new ArrayList<>();
+    private List<UpdateElement> uninstalls = new ArrayList<>();
 
-    public void searchNewAndUpdateModules() {
-        for (UpdateUnitProvider provider : UpdateUnitProviderFactory.getDefault().getUpdateUnitProviders(true)) {
-            try {
-                provider.refresh(null, true);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
+    public void findNewAndUpdateModules() {
+	localInstalled = getLocalInstalled();
 
-        for (UpdateUnit unit : UpdateManager.getDefault().getUpdateUnits()) {
-            if (!unit.getAvailableUpdates().isEmpty()) {
-                if (unit.getInstalled() == null) {
-					System.out.println("Found a new module " + unit.getCodeName());
-                    install.add(unit.getAvailableUpdates().get(0));
-                } else {
-					System.out.println("Found an update " + unit.getCodeName());
-                    update.add(unit.getAvailableUpdates().get(0));
-                }
-            }
-        }
+	for (UpdateUnitProvider provider
+		: UpdateUnitProviderFactory.getDefault()
+			.getUpdateUnitProviders(true)) {
+	    try {
+		provider.refresh(null, true);
+	    } catch (IOException ex) {
+		ex.printStackTrace();
+	    }
+	}
 
+	for (UpdateUnit unit : UpdateManager.getDefault().getUpdateUnits()) {
+	    if (!unit.getAvailableUpdates().isEmpty()) {
+		if (unit.getInstalled() == null) {
+		    System.out.println("Found a new module " + unit.getCodeName());
+		    install.add(unit.getAvailableUpdates().get(0));
+		} else {
+		    System.out.println("Found an update " + unit.getCodeName());
+		    update.add(unit.getAvailableUpdates().get(0));
+
+		}
+	    }
+	}
+    }
+
+    private List<UpdateElement> getLocalInstalled() {
+	List<UpdateElement> localInstalled = new ArrayList<>();
+	for (UpdateUnit unit : UpdateManager.getDefault().getUpdateUnits()) {
+	    if (unit.getInstalled() != null) {
+		localInstalled.add(unit.getInstalled());
+	    }
+	}
+	return localInstalled;
     }
 
     public OperationContainer<InstallSupport> addToContainer(
-            OperationContainer<InstallSupport> container,
-            List<UpdateElement> modules) throws Exception {
-        for (UpdateElement e : modules) {
-            if (container.canBeAdded(e.getUpdateUnit(), e)) {
-                OperationInfo<InstallSupport> operationInfo = container.add(e);
-                if (operationInfo != null) {
-                    container.add(operationInfo.getRequiredElements());
-                    if(!operationInfo.getBrokenDependencies().isEmpty()){
-                        throw new Exception("YOU FUCKED UP!");
-                    }
-                }
-            }
-        }
-        return container;
+	    OperationContainer<InstallSupport> container,
+	    List<UpdateElement> modules) throws Exception {
+	for (UpdateElement e : modules) {
+	    if (container.canBeAdded(e.getUpdateUnit(), e)) {
+		OperationInfo<InstallSupport> operationInfo = container.add(e);
+		if (operationInfo != null) {
+		    container.add(operationInfo.getRequiredElements());
+		    if (!operationInfo.getBrokenDependencies().isEmpty()) {
+			throw new Exception("YOU FUCKED UP!");
+		    }
+		}
+	    }
+	}
+	return container;
     }
 
     public void installModules(OperationContainer<InstallSupport> container) {
-        try {
-            InstallSupport support = container.getSupport();
-            if (support != null) {
-                Validator vali = support.doDownload(null, true, true);
-                Installer inst = support.doValidate(vali, null);
-                Restarter restarter = support.doInstall(inst, null);
-                if (restarter != null) {
-                    support.doRestartLater(restarter);
-                }
-            }
-        } catch (OperationException ex) {
-            ex.printStackTrace();
-        }
+	try {
+	    InstallSupport support = container.getSupport();
+	    if (support != null) {
+		Validator vali = support.doDownload(null, true, true);
+		Installer inst = support.doValidate(vali, null);
+		Restarter restarter = support.doInstall(inst, null);
+		if (restarter != null) {
+		    support.doRestartLater(restarter);
+		}
+	    }
+	} catch (OperationException ex) {
+	    ex.printStackTrace();
+	}
     }
 
     public List<UpdateElement> getInstall() {
-        return install;
+	return install;
     }
 
     public List<UpdateElement> getUpdate() {
-        return update;
+	return update;
     }
-    
-    
+
+    public void findModulesToUninstall() {
+	//Get installed modules from UC, and local installed
+	List<UpdateElement> modulesInSystemAndUC = getLocalInstalled();
+
+	//Get modules that is already installed 
+	List<UpdateElement> installedModules = new ArrayList<>(localInstalled);
+
+	//Remove all modules, that is local, but not in updatecenter.
+	installedModules.removeAll(modulesInSystemAndUC);
+	uninstalls = installedModules;
+    }
+
+    public OperationContainer<OperationSupport> uninstallModules() {
+	OperationContainer<OperationSupport> container = OperationContainer.createForDirectUninstall();
+
+	for (UpdateElement module : uninstalls) {
+	    if (container.canBeAdded(module.getUpdateUnit(), module)) {
+		OperationInfo<OperationSupport> info = container.add(module);
+		if (info == null) {
+		    continue;
+		}
+		container.add(info.getRequiredElements());
+	    }
+	}
+
+	if (!container.listAll().isEmpty()) {
+	    try {
+		//Uninstalls the module
+		Restarter rs = container.getSupport().doOperation(null);
+		if (rs != null) {
+		    //Restart module if necessary
+		    container.getSupport().doRestart(rs, null);
+		}
+	    } catch (OperationException ex) {
+		Exceptions.printStackTrace(ex);
+	    }
+	}
+
+	return container;
+    }
 
 }
