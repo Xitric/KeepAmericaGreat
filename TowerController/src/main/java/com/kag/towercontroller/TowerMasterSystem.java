ae9c9743c8700e3e95b2668359633c545175c181
Unknown <sofie.b.jorgensen@gmail.com>
2018-03-23 13:36:16 +0100

    Consumer added to TowerMasterSystem

--- TowerController/src/main/java/com/kag/towercontroller/TowerMasterSystem.java
+++ TowerController/src/main/java/com/kag/towercontroller/TowerMasterSystem.java
@@ -14,6 +14,7 @@ import com.kag.common.spinterfaces.IAssetManager;
 import com.kag.common.spinterfaces.IComponentLoader;
 import com.kag.common.spinterfaces.ISystem;
 import com.kag.interfaces.ITower;
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.openide.util.Lookup;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
@@ -28,8 +29,8 @@ import org.openide.util.LookupEvent;
 import org.openide.util.LookupListener;
 
 @ServiceProviders(value = {
-    @ServiceProvider(service = ISystem.class),
-    @ServiceProvider(service = IComponentLoader.class)
+        @ServiceProvider(service = ISystem.class),
+        @ServiceProvider(service = IComponentLoader.class)
 })
 public class TowerMasterSystem implements ISystem, IComponentLoader {
 
@@ -40,11 +41,21 @@ public class TowerMasterSystem implements ISystem, IComponentLoader {
     private List<ITower> towerImple;
     private Lookup.Result<ITower> towerImpleLookupResult;
     private List<Entity> towersToBeDrawn;
+    private List<Consumer<World>> towerConsumer;
+
+    public TowerMasterSystem() {
+        towerConsumer = new ArrayList<>();
+    }
 
     @Override
     public void update(float dt, World world) {
-        
 
+        if (!towerConsumer.isEmpty()) {
+            for (Consumer<World> consumer : towerConsumer) {
+                consumer.accept(world);
+            }
+            towerConsumer.clear();
+        }
     }
 
     @Override
@@ -60,8 +71,11 @@ public class TowerMasterSystem implements ISystem, IComponentLoader {
 
         towerImple = new CopyOnWriteArrayList<>();
         towerImpleLookupResult = lookup.lookupResult(ITower.class);
-	    towerImpleLookupResult.addLookupListener(iTowerLookupListener);
-        
+        towerImpleLookupResult.addLookupListener(iTowerLookupListener);
+        lookup.lookupAll(ITower.class).stream().map(this::addNewTowerToMenu).forEach((e) -> {
+            towersToBeDrawn.add(e);
+            System.out.println("HELLOEEEEEEEEEEEEEEEEEEEEEEEEEH");
+        });
         IAssetManager assetManager = Lookup.getDefault().lookup(IAssetManager.class);
 
         System.out.println("TOWER SYSTEM IS CALLED!!!");
@@ -73,9 +87,9 @@ public class TowerMasterSystem implements ISystem, IComponentLoader {
         upgradeMenuBackground.addPart(new MenuBackgroundPart(assetManager.createAsset(getClass().getResourceAsStream("/todo2.png"))));
         upgradeMenuBackground.addPart(new PositionPart(768, 0));
 
-        world.addEntity(towerMenuBackground);
+    //    world.addEntity(towerMenuBackground);
         world.addEntity(upgradeMenuBackground);
-       
+
     }
 
     @Override
@@ -84,49 +98,52 @@ public class TowerMasterSystem implements ISystem, IComponentLoader {
         world.removeEntity(upgradeMenuBackground);
     }
 
-    
-    private Entity addNewTowerToMenu(ITower tower){
+
+    private Entity addNewTowerToMenu(ITower tower) {
         //Create Entity from tower and return
         Entity towerEntity = new Entity();
         towersToBeDrawn.add(towerEntity);
 
-        for (Entity entity : towersToBeDrawn) {
-            if (entity == towerEntity) {
-                int index = towersToBeDrawn.indexOf(entity);
+        int index = towersToBeDrawn.indexOf(towerEntity);
 
-            }
-        }
+        int x = index % 3;
+        int y = index / 3;
+
+        int pixelx = 775 + x * 58;
+        int pixely = 500 + y * 58;
+
+        System.out.println("X:" + pixelx + "Y:" +pixely);
 
         IconPart iconPart = new IconPart(tower.getAsset());
-        PositionPart positionPart = new PositionPart(0, 0);
+        PositionPart positionPart = new PositionPart(pixelx, pixely);
 
         towerEntity.addPart(iconPart);
         towerEntity.addPart(positionPart);
 
-
         return towerEntity;
     }
-    
+
     private final LookupListener iTowerLookupListener = new LookupListener() {
         @Override
         public void resultChanged(LookupEvent ev) {
             Collection<? extends ITower> actualTowers = towerImpleLookupResult.allInstances();
-            Consumer<World> worldConsumer = (world) -> {
-
-            };
 
             for (ITower tower : actualTowers) {
                 // Newly installed modules
+                System.out.println("NU FINDER VI SGU ITOWERS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
                 if (!towerImple.contains(tower)) {
                     towerImple.add(tower);
-                    addNewTowerToMenu(tower);
+                    Entity entity = addNewTowerToMenu(tower);
+                    towerConsumer.add(world -> {
+                       world.addEntity(entity);
+                    });
                 }
             }
             // Stop and remove module
             for (ITower tower : towerImple) {
                 if (!actualTowers.contains(tower)) {
                     towerImple.remove(tower);
-                    addNewTowerToMenu(tower);
                 }
             }
         }
