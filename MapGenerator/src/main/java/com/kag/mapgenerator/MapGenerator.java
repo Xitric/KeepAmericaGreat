6c7bde5c1d0ec46dcf85b624ab5d5d37d27c6c75
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2018-03-23 08:51:17 +0100

    Added bridge builder and optimized water builder slightly
    The water builder is still not perfect for small octave counts.

--- MapGenerator/src/main/java/com/kag/mapgenerator/MapGenerator.java
+++ MapGenerator/src/main/java/com/kag/mapgenerator/MapGenerator.java
@@ -4,74 +4,72 @@ import com.kag.common.data.GameMap;
 import com.kag.common.data.IAsset;
 import com.kag.common.spinterfaces.IAssetManager;
 import com.kag.common.spinterfaces.IMapGenerator;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.ServiceProvider;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.openide.util.Lookup;
-import org.openide.util.lookup.ServiceProvider;
-
 @ServiceProvider(service = IMapGenerator.class)
 public class MapGenerator implements IMapGenerator {
 
-    private int width;
-    private int height;
-    private final int octaves = 3;
-    private List<IWorldBuilder> worldBuilders;
+	private final static int octaves = 3;
+	private int width;
+	private int height;
+	private List<IWorldBuilder> worldBuilders;
 
-    public MapGenerator() {
-        //Not to be used.
-    }
+	public MapGenerator() {
+		//Not to be used.
+	}
 
-    private MapGenerator(int width, int height) {
-        this.width = width;
-        this.height = height;
-        worldBuilders = new ArrayList<>();
-    }
+	private MapGenerator(int width, int height) {
+		this.width = width;
+		this.height = height;
+		worldBuilders = new ArrayList<>();
+	}
 
-    private static MapGenerator newWorld(int width, int height) {
-        return new MapGenerator(width, height);
-    }
+	private static MapGenerator newWorld(int width, int height) {
+		return new MapGenerator(width, height);
+	}
 
-    @Override
-    public GameMap generateMap(int width, int height) {
-        return MapGenerator.newWorld(width, height)
-                .using(new GroundBuilder())
-                .using(new WaterBuilder())
-                .using(new SpawnBuilder())
-                .using(new ObstacleBuilder())
-                .create();
-    }
+	@Override
+	public GameMap generateMap(int width, int height) {
+		return MapGenerator.newWorld(width, height)
+				.using(new WaterBuilder())
+				.using(new SpawnBuilder())
+				.using(new BridgeBuilder())
+				.using(new ObstacleBuilder())
+				.create();
+	}
 
-    private MapGenerator using(IWorldBuilder worldBuilder) {
-        worldBuilders.add(worldBuilder);
-        return this;
-    }
+	private MapGenerator using(IWorldBuilder worldBuilder) {
+		worldBuilders.add(worldBuilder);
+		return this;
+	}
 
-    private GameMap create() {
-        IAssetManager assetManager = Lookup.getDefault().lookup(IAssetManager.class);
-        IAsset asset = assetManager.createAsset(getClass().getResourceAsStream("/tilesheet.png"));
-        GameMap gameMap;
+	private GameMap create() {
+		IAssetManager assetManager = Lookup.getDefault().lookup(IAssetManager.class);
+		IAsset asset = assetManager.createAsset(getClass().getResourceAsStream("/tilesheet.png"));
+		GameMap gameMap;
 
-        boolean failed;
-        do {
-            failed = false;
-            gameMap = new GameMap(width, height);
-            float[][] whiteNoise = PerlinNoiseGenerator.generateWhiteNoise(gameMap.getHeight(), gameMap.getWidth());
-            float[][] heightMap = PerlinNoiseGenerator.generateSmoothNoise(whiteNoise, octaves);
+		boolean failed;
+		do {
+			failed = false;
+			gameMap = new GameMap(width, height);
+			float[][] whiteNoise = PerlinNoiseGenerator.generateWhiteNoise(gameMap.getHeight(), gameMap.getWidth());
+			float[][] heightMap = PerlinNoiseGenerator.generateSmoothNoise(whiteNoise, octaves);
 
-            for (IWorldBuilder worldBuilder : worldBuilders) {
-                if (!worldBuilder.build(heightMap, gameMap)) {
-                    //Map failed to build, create a new one
-                    failed = true;
-                    break;
-                }
-            }
+			for (IWorldBuilder worldBuilder : worldBuilders) {
+				if (!worldBuilder.build(heightMap, gameMap)) {
+					//Map failed to build, create a new one
+					failed = true;
+					break;
+				}
+			}
 
-        } while (failed);
+		} while (failed);
 
-        gameMap.setSpriteSheet(asset);
-        return gameMap;
-
-    }
+		gameMap.setSpriteSheet(asset);
+		return gameMap;
+	}
 }
