23790ca8f67fac13cf90c3d0f81a38cb387434e3
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2018-03-24 20:13:45 +0100

    Improved enemy movement

---
+++ Common/src/main/java/com/kag/common/data/math/Vector2f.java
@@ -0,0 +1,205 @@
+package com.kag.common.data.math;
+
+/**
+ * An immutable representation of the mathematical two-dimensional vector.
+ * 
+ * @author Kasper
+ *
+ */
+public class Vector2f {
+	
+	/**
+	 * The zero vector.
+	 */
+	public static final Vector2f ZERO = new Vector2f(0.0f, 0.0f);
+	
+	/**
+	 * An axis.
+	 */
+	public static final Vector2f
+		AXIS_X = new Vector2f(1.0f, 0.0f),
+		AXIS_Y  = new Vector2f(0.0f, 1.0f);
+
+	public final float x, y;
+	
+	/**
+	 * Constructs a new zero vector.
+	 */
+	public Vector2f() {
+		this(0.0f, 0.0f);
+	}
+	
+	/**
+	 * Constructs a new vector with its content equal to that of another
+	 * vector.
+	 * 
+	 * @param other the vector to copy
+	 */
+	public Vector2f(Vector2f other) {
+		this(other.x, other.y);
+	}
+	
+	/**
+	 * Constructs a new vector with initial values.
+	 * 
+	 * @param x the x-component
+	 * @param y the y-component
+	 */
+	public Vector2f(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * Get the length of this vector. For optimization purposes, consider using
+	 * {@link #lengthSquared()} instead.
+	 *
+	 * @return the length of this vector
+	 */
+	public float length() {
+		return (float) Math.sqrt(lengthSquared());
+	}
+
+	/**
+	 * Get the squared length of this vector.
+	 *
+	 * @return the squared length of this vector
+	 */
+	public float lengthSquared() {
+		return this.x * this.x + this.y * this.y;
+	}
+
+	/**
+	 * Get the negated version of this vector. That is, invert the sign of all
+	 * coordinates.
+	 *
+	 * @return the negated version of this vector
+	 */
+	public Vector2f negate() {
+		return new Vector2f(- this.x, - this.y);
+	}
+
+	/**
+	 * Get the normalized version of this vector.
+	 *
+	 * @return the normalized version of this vector
+	 * @throws IllegalStateException If the vector has a length of zero
+	 */
+	public Vector2f normalize() {
+		float len = length();
+
+		if (len != 0.0f) {
+			return this.scale(1.0f / length());
+		}
+
+		//Length is zero
+		throw new IllegalStateException("Cannot normalize zero length vector");
+	}
+
+	/**
+	 * Test whether or not this is a unit vector. That is, whether its length
+	 * or squared length is very close to one. This function allows for a
+	 * deviation of {@code 0.000001} units to take into account floating point
+	 * errors.
+	 *
+	 * @return {@code true} if it is a unit vector, {@code false} otherwise
+	 */
+	public boolean isUnit() {
+		float lenSq = lengthSquared();
+		return lenSq > 0.999999f && lenSq < 1.000001f;
+	}
+
+	/**
+	 * Test whether or not this is a zero vector. That is, whether its length
+	 * or squared length is very close to zero. This function allows for a
+	 * deviation of {@code 0.000001} units to take into account floating point
+	 * errors.
+	 *
+	 * @return {@code true} if it is a zero vector, {@code false} otherwise
+	 */
+	public boolean isZero() {
+		float lenSq = lengthSquared();
+		return lenSq < 0.000001f;
+	}
+
+	/**
+	 * Get a version of this vector that has been scaled by a given magnitude.
+	 *
+	 * @param scale the magnitude to scale the vector by
+	 * @return a scaled version of this vector
+	 */
+	public Vector2f scale(float scale) {
+		return new Vector2f(this.x * scale, this.y * scale);
+	}
+	
+	/**
+	 * Add another vector to this vector. A new vector is returned.
+	 * 
+	 * @param other the vector to add
+	 * @return the result of the addition as a new vector
+	 */
+	public Vector2f add(Vector2f other) {
+		return new Vector2f(this.x + other.x, this.y + other.y);
+	}
+	
+	/**
+	 * Subtract another vector from this vector. A new vector is returned.
+	 * 
+	 * @param other the vector to subtract
+	 * @return the result of the subtraction as a new vector
+	 */
+	public Vector2f sub(Vector2f other) {
+		return new Vector2f(this.x - other.x, this.y - other.y);
+	}
+	
+	/**
+	 * Get the dot product between this vector and another vector.
+	 * 
+	 * @param other the vector to dot with
+	 * @return the result of the dot product
+	 */
+	public float dot(Vector2f other) {
+		return this.x * other.x +
+				this.y * other.y;
+	}
+	
+	/**
+	 * Get the determinant between this vector and another vector. The order is
+	 * {@code det(this, other)}.
+	 * 
+	 * @param other the other vector
+	 * @return the scalar result of the operation
+	 */
+	public float det(Vector2f other) {
+		return this.x * other.y - other.x * this.y;
+	}
+	
+	/**
+	 * Get the result of rotating this vector 90 degrees ccw around the z-axis
+	 * in a left-handed coordinate system.
+	 * 
+	 * @return the result of rotating this vector 90 degrees ccw around the
+	 * z-axis in a left-handed coordinate system
+	 */
+	public Vector2f getLeftHandCCW() {
+		return new Vector2f(this.y, - this.x);
+	}
+	
+	/**
+	 * Get the result of rotating this vector 90 degrees ccw around the z-axis
+	 * in a right-handed coordinate system.
+	 * 
+	 * @return the result of rotating this vector 90 degrees ccw around the
+	 * z-axis in a right-handed coordinate system
+	 */
+	public Vector2f getRightHandCCW() {
+		return new Vector2f(- this.y, this.x);
+	}
+	
+	@Override
+	public String toString() {
+		return "Vector2f: (" +
+				x + ", " +
+				y + ")";
+	}
+}
--- EnemyController/src/main/java/com/kag/enemycontroller/EnemyMovingSystem.java
+++ EnemyController/src/main/java/com/kag/enemycontroller/EnemyMovingSystem.java
@@ -1,8 +1,10 @@
 package com.kag.enemycontroller;
 
 import com.kag.common.data.GameData;
+import com.kag.common.data.GameMap;
 import com.kag.common.data.Node;
 import com.kag.common.data.World;
+import com.kag.common.data.math.Vector2f;
 import com.kag.common.entities.Entity;
 import com.kag.common.entities.Family;
 import com.kag.common.entities.parts.PositionPart;
@@ -21,44 +23,68 @@ public class EnemyMovingSystem implements IEntitySystem {
 	private static final Family FAMILY = Family.forAll(EnemyPart.class, PositionPart.class);
 
 	@Override
-	public Family getFamily() {
-		return FAMILY;
+	public void update(float delta, Entity entity, World world, GameData gameData) {
+		EnemyPart enemyPart = entity.getPart(EnemyPart.class);
+
+		if (enemyPart.getNextNode() == null) {
+			generateNewPath(entity, world);
+		}
+
+		if (enemyPart.getNextNode() != null) {
+			move(entity, delta);
+		}
+	}
+
+	private void generateNewPath(Entity entity, World world) {
+		//TODO: Just look it up in the game map when Dijkstra has been implemented
+		IPathFinder pathFinder = Lookup.getDefault().lookup(IPathFinder.class);
+		if (pathFinder == null) return;
+
+		GameMap map = world.getGameMap();
+
+		PositionPart positionPart = entity.getPart(PositionPart.class);
+		int tileX = (int) (positionPart.getX() / map.getTileWidth());
+		int tileY = (int) (positionPart.getY() / map.getTileHeight());
+
+		Node nextNode = pathFinder.getPath(tileX, tileY, map.getPlayerX(), map.getPlayerY(), world);
+		entity.getPart(EnemyPart.class).setNextNode(nextNode);
+	}
+
+	private void move(Entity entity, float dt) {
+		PositionPart positionPart = entity.getPart(PositionPart.class);
+		EnemyPart enemyPart = entity.getPart(EnemyPart.class);
+		Node nextNode = enemyPart.getNextNode();
+
+		Vector2f position = new Vector2f(positionPart.getX(), positionPart.getY());
+		Vector2f goal = new Vector2f(nextNode.getTile().getX() * 64, nextNode.getTile().getY() * 64);
+
+		Vector2f moveDirection = goal.sub(position);
+		Vector2f move = Vector2f.ZERO;
+		if (!moveDirection.isZero()) {
+			move = moveDirection.normalize().scale(enemyPart.getSpeed() * dt);
+		}
+
+		if (move.lengthSquared() > moveDirection.lengthSquared()) {
+			//We are able to reach past the current goal node, so we check if we can go to the next node instead
+			nextNode = nextNode.getNext();
+			enemyPart.setNextNode(nextNode);
+
+			if (nextNode == null) {
+				//We reached the final goal, so just go to it
+				move = moveDirection;
+			} else {
+				goal = new Vector2f(nextNode.getTile().getX() * 64, nextNode.getTile().getY() * 64);
+				move = goal.sub(position).normalize().scale(enemyPart.getSpeed() * dt);
+			}
+		}
+
+		Vector2f newPosition = position.add(move);
+		positionPart.setPos(newPosition.x, newPosition.y);
 	}
 
 	@Override
-	public void update(float delta, Entity entity, World world, GameData gameData) {
-		EnemyPart enemyPart = entity.getPart(EnemyPart.class);
-		PositionPart positionPart = entity.getPart(PositionPart.class);
-
-		if (enemyPart.getNextNode() == null) {
-			IPathFinder pathfinder = Lookup.getDefault().lookup(IPathFinder.class);
-			if (pathfinder != null) {
-				Node nextNode = pathfinder.getPath((int) positionPart.getX() / 64, (int) positionPart.getY() / 64, 6, 35, world);
-				if (nextNode == null) System.out.println("No valid path!!!");
-				enemyPart.setNextNode(nextNode);
-			}
-		}
-
-		Node nextNode = enemyPart.getNextNode();
-
-		if (nextNode != null) {
-			float dx = nextNode.getTile().getX() * 64 - positionPart.getX();
-			float dy = nextNode.getTile().getY() * 64 - positionPart.getY();
-
-			if (dx != 0) dx /= Math.abs(dx);
-			if (dy != 0) dy /= Math.abs(dy);
-
-			//TODO: Can be a lot better - we should consider using vectors ;)
-			positionPart.setPos(positionPart.getX() + delta * dx * enemyPart.getSpeed(), positionPart.getY() + delta * dy * enemyPart.getSpeed());
-
-			if (atNode(positionPart, nextNode)) {
-				enemyPart.setNextNode(nextNode.getNext());
-			}
-		}
-	}
-
-	private boolean atNode(PositionPart position, Node node) {
-		return Math.abs(position.getX() - node.getTile().getX() * 64) + Math.abs(position.getY() - node.getTile().getY() * 64) < 5;
+	public Family getFamily() {
+		return FAMILY;
 	}
 
 	@Override
