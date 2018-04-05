e949f4adb473b02e5ac5b67a2227378e40b274fe
Niels Lauritsen <nmlthedane@gmail.com>
2018-04-05 14:12:25 +0200

    More Refactor

--- TowerController/src/main/java/com/kag/towercontroller/TowerSelectionManager.java
+++ TowerController/src/main/java/com/kag/towercontroller/TowerSelectionManager.java
@@ -66,24 +66,22 @@ public class TowerSelectionManager {
 
     public void updateOverlay(World world, GameData gameData){
         float xTilePositionOnMap = (gameData.getCamera().getX() - gameData.getWidth() / 2 + gameData.getMouse().getX()) / 64;
-        float yTolePositionOnMap = (gameData.getCamera().getY() - gameData.getHeight() / 2 + gameData.getMouse().getY()) / 64;
+        float yTilePositionOnMap = (gameData.getCamera().getY() - gameData.getHeight() / 2 + gameData.getMouse().getY()) / 64;
 
-        Tile hoverTile = world.getGameMap().getTile((int) xTilePositionOnMap, (int) yTolePositionOnMap);
-        getMouseTower().getPart(AbsolutePositionPart.class).setPos(hoverTile.getX() * 64, hoverTile.getY() * 64);
+        Tile hoverTile = world.getGameMap().getTile((int) xTilePositionOnMap, (int) yTilePositionOnMap);
+        getMouseTower().getPart(AbsolutePositionPart.class).setPos(hoverTile.getX() * 64 - gameData.getCamera().getX() + (gameData.getWidth()/2), hoverTile.getY() * 64 - gameData.getCamera().getY() + (gameData.getHeight()/2));
 
         if (!world.isOccupied(hoverTile.getX(), hoverTile.getY())) {
             getMouseTower().removePart(overlayAssetPart);
             overlayAssetPart = assetManager.createTexture(getClass().getResourceAsStream("/BlueOverlay.png"));
             overlayAssetPart.setzIndex(30);
             getMouseTower().addPart(overlayAssetPart);
-            System.out.println("Ledig!");
 
         } else {
             getMouseTower().removePart(overlayAssetPart);
             overlayAssetPart = assetManager.createTexture(getClass().getResourceAsStream("/RedOverlay.png"));
             overlayAssetPart.setzIndex(30);
             getMouseTower().addPart(overlayAssetPart);
-            System.out.println("Optaget!");
         }
     }
 
@@ -100,8 +98,9 @@ public class TowerSelectionManager {
 
             world.removeEntity(getMouseTower());
             setMouseTower(null);
+            setSelectedTower(null);
             world.addEntity(newTower);
-
+            hoverTile.setWalkable(false);
         }
     }
 }
