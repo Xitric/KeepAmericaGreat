3d645309d02b7522deca8b220fdd0d57feea554f
Mikkel Struntze <mikkel.struntze@gmail.com>
2018-04-06 12:15:37 +0200

    Path validation for tower placement

--- TowerController/src/main/java/com/kag/towercontroller/TowerSelectionManager.java
+++ TowerController/src/main/java/com/kag/towercontroller/TowerSelectionManager.java
@@ -1,14 +1,12 @@
 package com.kag.towercontroller;
 
-import com.kag.common.data.GameData;
-import com.kag.common.data.IAsset;
-import com.kag.common.data.Tile;
-import com.kag.common.data.World;
+import com.kag.common.data.*;
 import com.kag.common.entities.Entity;
 import com.kag.common.entities.parts.AbsolutePositionPart;
 import com.kag.common.entities.parts.AssetPart;
 import com.kag.common.entities.parts.PositionPart;
 import com.kag.common.spinterfaces.IAssetManager;
+import com.kag.common.spinterfaces.IPathFinder;
 import org.openide.util.Lookup;
 
 public class TowerSelectionManager {
@@ -69,6 +67,26 @@ public class TowerSelectionManager {
         }
     }
 
+    private Node[][] getPaths(World world) {
+	    IPathFinder pathFinder = Lookup.getDefault().lookup(IPathFinder.class);
+
+	    return pathFinder.getPath(world.getGameMap().getPlayerX(), world.getGameMap().getPlayerY(), world);
+    }
+
+    private int countValidPaths(Node[][] paths) {
+	    int count = 0;
+
+	    for (int i = 0; i < paths.length; i++) {
+	    	for(int j = 0; j < paths[i].length; j++) {
+	    		if(paths[i][j] != null) {
+	    			count++;
+			    }
+		    }
+	    }
+
+    	return count;
+    }
+
     public void placeTowerOnGameMap(World world, GameData gameData) {
         float xTilePositionOnMap = (gameData.getCamera().getX() - gameData.getWidth() / 2 + gameData.getMouse().getX()) / 64;
         float yTilePositionOnMap = (gameData.getCamera().getY() - gameData.getHeight() / 2 + gameData.getMouse().getY()) / 64;
@@ -81,9 +99,49 @@ public class TowerSelectionManager {
             newTower.getPart(PositionPart.class).setPos(hoverTile.getX() * 64, hoverTile.getY() * 64);
 
             resetTowerSelection(world);
+
+            // Running Dijkstra pf to get a count of nodes available for start pos, to know how many start positions are removed after tower placement
+	        Node[][] validPaths = getPaths(world);
+	        int validPathsCount = countValidPaths(validPaths);
+
             world.addEntity(newTower);
             hoverTile.setWalkable(false);
+
+            if(!isMapValid(world, validPathsCount, validPaths)) {
+            	world.removeEntity(newTower);
+            	hoverTile.setWalkable(true);
+            }
         }
+    }
+
+    private boolean isMapValid(World world, int prevValidPathsCount, Node[][] prevValidPaths) {
+    	/*
+    	Run Dijkstra.
+    	If more than one possible path to the goal is removed, an area is blocked off.
+    	Validate that there are no enemies by checking the blocked off tiles for whether they are walkable and occupied,
+    	if that's the case, there are enemies on the blocked off tiles and the map is invalid.
+    	 */
+	    IPathFinder pathFinder = Lookup.getDefault().lookup(IPathFinder.class);
+
+	    // Check whether there's a valid path from spawn to goal (the player)
+		if(pathFinder.getPath(0,0, world.getGameMap().getPlayerX(), world.getGameMap().getPlayerY(), world) == null) {
+			return false;
+		}
+
+		Node[][] validPaths = getPaths(world);
+
+		// Check whether more than one valid path was removed
+		if(prevValidPathsCount - countValidPaths(validPaths) > 1) {
+			for (int y = 0; y < validPaths.length; y++) {
+				for (int x = 0; x < validPaths[y].length; x++) {
+					if(prevValidPaths[y][x] != null && validPaths[y][x] == null && world.isWalkable(x, y) && world.isOccupied(x, y)) {
+						return false;
+					}
+				}
+			}
+		}
+
+		return true;
     }
 
     public void resetTowerSelection(World world) {
