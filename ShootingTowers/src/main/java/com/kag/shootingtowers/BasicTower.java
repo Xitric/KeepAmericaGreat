f625767f12921119743e9a2742cbe0f9d3698bca
André Thy <2809thy@gmail.com>
2018-04-10 12:51:07 +0200

    Started using rotation speed

--- ShootingTowers/src/main/java/com/kag/shootingtowers/BasicTower.java
+++ ShootingTowers/src/main/java/com/kag/shootingtowers/BasicTower.java
@@ -30,7 +30,7 @@ public class BasicTower implements ITower {
 				.setAttackSpeed(0.25f)
 				.setProjectileSpeed(200)
 				.setCost(20)
-				.setRotationSpeed((float) Math.PI / 4)
+				.setRotationSpeed(20)
 				.setBaseAsset(baseAsset)
 				.setTurretAsset(turretAsset)
 				.setTurretAxisX(turretAsset.getHeight() / 2)
--- ShootingTowers/src/main/java/com/kag/shootingtowers/ShootingTowerControlSystem.java
+++ ShootingTowers/src/main/java/com/kag/shootingtowers/ShootingTowerControlSystem.java
@@ -22,122 +22,140 @@ import org.openide.util.lookup.ServiceProviders;
 import java.util.Collection;
 
 @ServiceProviders(value = {
-		@ServiceProvider(service = IEntitySystem.class),
-		@ServiceProvider(service = IComponentLoader.class)
+        @ServiceProvider(service = IEntitySystem.class),
+        @ServiceProvider(service = IComponentLoader.class)
 })
 public class ShootingTowerControlSystem implements IEntitySystem, IComponentLoader {
 
-	private static final Family FAMILY = Family.forAll(NamePart.class, PositionPart.class, BlockingPart.class, RotationSpeedPart.class, CostPart.class, WeaponPart.class);
-	private static final Family ENEMY_FAMILY = Family.forAll(LifePart.class, PositionPart.class, BoundingBoxPart.class, BlockingPart.class);
-	private IProjectile projectileImplementation;
-	private IAsset projectileAsset;
+    private static final Family FAMILY = Family.forAll(NamePart.class, PositionPart.class, BlockingPart.class, RotationSpeedPart.class, CostPart.class, WeaponPart.class);
+    private static final Family ENEMY_FAMILY = Family.forAll(LifePart.class, PositionPart.class, BoundingBoxPart.class, BlockingPart.class);
+    private IProjectile projectileImplementation;
+    private IAsset projectileAsset;
 
-	@Override
-	public void load(World world) {
-		IAssetManager assetManager = Lookup.getDefault().lookup(IAssetManager.class);
-		projectileAsset = assetManager.loadAsset(getClass().getResourceAsStream("/Missile.png"));
-	}
+    @Override
+    public void load(World world) {
+        IAssetManager assetManager = Lookup.getDefault().lookup(IAssetManager.class);
+        projectileAsset = assetManager.loadAsset(getClass().getResourceAsStream("/Missile.png"));
+    }
 
-	@Override
-	public void dispose(World world) {
-		for (Entity entity : ShootingTowerBuilder.getAllTowers()) {
-			world.removeEntity(entity);
-		}
-		projectileAsset.dispose();
-	}
+    @Override
+    public void dispose(World world) {
+        for (Entity entity : ShootingTowerBuilder.getAllTowers()) {
+            world.removeEntity(entity);
+        }
+        projectileAsset.dispose();
+    }
 
-	@Override
-	public Family getFamily() {
-		return FAMILY;
-	}
+    @Override
+    public Family getFamily() {
+        return FAMILY;
+    }
 
-	@Override
-	public void update(float delta, Entity entity, World world, GameData gameData) {
-		WeaponPart weaponPart = entity.getPart(WeaponPart.class);
-		weaponPart.addDelta(delta);
+    @Override
+    public void update(float delta, Entity entity, World world, GameData gameData) {
+        WeaponPart weaponPart = entity.getPart(WeaponPart.class);
+        weaponPart.addDelta(delta);
 
-		Entity enemy = getNearestEnemy(world, entity);
-		if (enemy != null){
-			rotateTower(entity, calculateRotation(enemy, entity));
-		}
+        Entity enemy = getNearestEnemy(world, entity);
+        if (enemy != null) {
+            rotateTower(entity, calculateRotationForTower(enemy, entity, delta));
+        }
 
-		float timeBetweenShot = 1 / weaponPart.getAttackSpeed();
-		if (weaponPart.getTimeSinceLast() > timeBetweenShot) {
-			if (enemy != null) {
-				shootAt(world, enemy, entity);
-				weaponPart.addDelta(-timeBetweenShot);
-			} else {
-				weaponPart.addDelta(timeBetweenShot - weaponPart.getTimeSinceLast());
-			}
-		}
-	}
+        float timeBetweenShot = 1 / weaponPart.getAttackSpeed();
+        if (weaponPart.getTimeSinceLast() > timeBetweenShot) {
+            if (enemy != null) {
+                shootAt(world, enemy, entity);
+                weaponPart.addDelta(-timeBetweenShot);
+            } else {
+                weaponPart.addDelta(timeBetweenShot - weaponPart.getTimeSinceLast());
+            }
+        }
+    }
 
-	/**
-	 * Get the nearest enemy to the specified tower. This method considers only enemies that are within the range of the
-	 * tower. If no enemy is close enough, this method will return null.
-	 *
-	 * @param world the game world
-	 * @param tower the tower from which to find the nearest enemy
-	 * @return the enemy nearest to the tower, or null if no enemies are in range
-	 */
-	private Entity getNearestEnemy(World world, Entity tower) {
-		PositionPart towerPositionPart = tower.getPart(PositionPart.class);
-		WeaponPart towerWeaponPart = tower.getPart(WeaponPart.class);
+    /**
+     * Get the nearest enemy to the specified tower. This method considers only enemies that are within the range of the
+     * tower. If no enemy is close enough, this method will return null.
+     *
+     * @param world the game world
+     * @param tower the tower from which to find the nearest enemy
+     * @return the enemy nearest to the tower, or null if no enemies are in range
+     */
+    private Entity getNearestEnemy(World world, Entity tower) {
+        PositionPart towerPositionPart = tower.getPart(PositionPart.class);
+        WeaponPart towerWeaponPart = tower.getPart(WeaponPart.class);
 
-		Entity nearest = null;
-		float shortestDist = towerWeaponPart.getRange();
+        Entity nearest = null;
+        float shortestDist = towerWeaponPart.getRange();
 
-		for (Entity enemy : world.getEntitiesByFamily(ENEMY_FAMILY)) {
-			PositionPart enemyPositionPart = enemy.getPart(PositionPart.class);
-			float distance = (float) Math.hypot(enemyPositionPart.getX() - towerPositionPart.getX(), enemyPositionPart.getY() - towerPositionPart.getY());
+        for (Entity enemy : world.getEntitiesByFamily(ENEMY_FAMILY)) {
+            PositionPart enemyPositionPart = enemy.getPart(PositionPart.class);
+            float distance = (float) Math.hypot(enemyPositionPart.getX() - towerPositionPart.getX(), enemyPositionPart.getY() - towerPositionPart.getY());
 
-			if (distance <= shortestDist) {
-				nearest = enemy;
-				shortestDist = distance;
-			}
-		}
+            if (distance <= shortestDist) {
+                nearest = enemy;
+                shortestDist = distance;
+            }
+        }
 
-		return nearest;
-	}
+        return nearest;
+    }
 
-	private void shootAt(World world, Entity enemy, Entity tower) {
-		projectileImplementation = Lookup.getDefault().lookup(IProjectile.class);
+    private void shootAt(World world, Entity enemy, Entity tower) {
+        projectileImplementation = Lookup.getDefault().lookup(IProjectile.class);
 
-		WeaponPart weaponPart = tower.getPart(WeaponPart.class);
-		PositionPart towerPositionPart = tower.getPart(PositionPart.class);
+        WeaponPart weaponPart = tower.getPart(WeaponPart.class);
+        PositionPart towerPositionPart = tower.getPart(PositionPart.class);
 
-		//Calculate rotation
-		float rotationResult = calculateRotation(enemy, tower);
+        //Calculate rotation
+        float rotationResult = calculateRotation(enemy, tower);
 
 
-		IAssetManager assetManager = Lookup.getDefault().lookup(IAssetManager.class);
-		AssetPart pAsset = assetManager.createTexture(projectileAsset, 0, 0, projectileAsset.getWidth(), projectileAsset.getHeight());
+        IAssetManager assetManager = Lookup.getDefault().lookup(IAssetManager.class);
+        AssetPart pAsset = assetManager.createTexture(projectileAsset, 0, 0, projectileAsset.getWidth(), projectileAsset.getHeight());
 
-		projectileImplementation.createProjectile(towerPositionPart.getX(), towerPositionPart.getY(), weaponPart.getDamage(), weaponPart.getProjectileSpeed(), rotationResult, world, pAsset);
-	}
+        projectileImplementation.createProjectile(towerPositionPart.getX(), towerPositionPart.getY(), weaponPart.getDamage(), weaponPart.getProjectileSpeed(), rotationResult, world, pAsset);
+    }
 
-	private void rotateTower(Entity tower, float rotationResult) {
-		Collection<? extends AssetPart> assetParts = tower.getPartsDeep(AssetPart.class);
+    private AssetPart getTurretPart(Entity tower) {
+        Collection<? extends AssetPart> assetParts = tower.getPartsDeep(AssetPart.class);
+        for (AssetPart assetPart : assetParts) {
+            if (assetPart.getzIndex() == ZIndex.TOWER_TURRET.value) {
+                return assetPart;
+            }
+        }
+        return null;
+    }
 
-		for(AssetPart assetPart : assetParts){
-			if(assetPart.getzIndex() == ZIndex.TOWER_TURRET.value){
-				assetPart.setRotation(rotationResult);
-			}
-		}
-	}
+    private void rotateTower(Entity tower, float rotationResult) {
+        //Only move tower according to rotationspeed
+        getTurretPart(tower).setRotation(rotationResult);
+    }
 
-	private float calculateRotation(Entity enemy, Entity tower) {
-		PositionPart enemyPositionPart = enemy.getPart(PositionPart.class);
-		PositionPart towerPositionPart = tower.getPart(PositionPart.class);
 
-		Vector2f move = new Vector2f(enemyPositionPart.getX() - towerPositionPart.getX(), enemyPositionPart.getY() - towerPositionPart.getY());
-		Vector2f lookDir = move.normalize();
-		float rotationPi = (float) Math.atan2(lookDir.det(Vector2f.AXIS_X), lookDir.dot(Vector2f.AXIS_X));
-		return -(float) (rotationPi / (2 * Math.PI) * 360);
-	}
+    private float calculateRotation(Entity enemy, Entity tower) {
+        PositionPart enemyPositionPart = enemy.getPart(PositionPart.class);
+        PositionPart towerPositionPart = tower.getPart(PositionPart.class);
 
-	@Override
-	public int getPriority() {
-		return 0;
-	}
+        Vector2f move = new Vector2f(enemyPositionPart.getX() - towerPositionPart.getX(), enemyPositionPart.getY() - towerPositionPart.getY());
+        Vector2f lookDir = move.normalize();
+        float rotationPi = (float) Math.atan2(lookDir.det(Vector2f.AXIS_X), lookDir.dot(Vector2f.AXIS_X));
+        return -(float) (rotationPi / (2 * Math.PI) * 360);
+    }
+
+    private float calculateRotationForTower(Entity enemy, Entity tower, float dt) {
+        float rotationSpeed = tower.getPart(RotationSpeedPart.class).getRotationSpeed();
+        float towerRotation = getTurretPart(tower).getRotation();
+        float rotationToEnemy = calculateRotation(enemy, tower);
+
+        float difference = rotationToEnemy - towerRotation;
+        float toRotate = Math.min(rotationSpeed * dt, difference);
+
+        System.out.println(towerRotation + toRotate);
+        return towerRotation + toRotate;
+    }
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
 }
